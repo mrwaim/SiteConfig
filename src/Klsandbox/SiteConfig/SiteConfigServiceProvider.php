9d3153bc61d37545a41e6ce915cd7527a6736dd4
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-29 21:07:06 +0800

    init

---
+++ src/Klsandbox/SiteConfig/SiteConfigServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Klsandbox\SiteConfig;
+
+use Illuminate\Support\ServiceProvider;
+
+class SiteConfigServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
